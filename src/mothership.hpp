497dd7a03cd8b893949303a6974933824d3941a5
William Wilson <wilsontfwi@gmail.com>
2022-11-24 23:06:27 -0600

    Adds Dear IMGUI to the project

--- src/mothership.hpp
+++ src/mothership.hpp
@@ -5,6 +5,9 @@
 #include <windows.h>
 #include <tchar.h>
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #define GLFW_INCLUDE_NONE
 #include "glad/gl.h"
 #include "GLFW/glfw3.h"
